f89c1413174550440a41c5d54740c0f481f8cd2f
CorainChicago <chaysmagan@gmail.com>
2016-01-19 13:10:24 -0600

    add comment

--- public/scripts/example.js
+++ public/scripts/example.js
@@ -1,8 +1,47 @@
+var Comment = React.createClass({
+  render: function(){
+    return (
+      <div className="comment">
+        <h2 className="commentAuthor">
+          {this.props.author}
+        </h2>
+        {this.props.children}
+      </div>
+    );
+  }
+});
+
+
+
+var CommentList = React.createClass({
+  render: function() {
+    return(
+      <div className="commentList">
+        <Comment author="Charlie Bear"> This is one comment from Charlie</Comment>
+        <Comment author="Alex Fedesna"> This is a comment from Alex. She is great</Comment>
+      </div>
+      );
+  }
+});
+
+var CommentForm = React.createClass({
+  render: function(){
+    return (
+      <div className="commentForm">
+      Hello Cats!  I am a CommentForm.
+      </div>
+      )
+  }
+})
+
 var CommentBox = React.createClass({
   render: function() {
     return (
       <div className="commentBox">
         Hello, world! I am a CommentBox.
+        <h2>Comments</h2>
+        <CommentList />
+        <CommentForm />
       </div>
     );
   }
@@ -11,3 +50,4 @@ ReactDOM.render(
   <CommentBox />,
   document.getElementById('content')
 );
+
